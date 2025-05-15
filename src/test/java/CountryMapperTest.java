f1433e3090b4169adf4df44ce73ab4e01f681df8
cn <1921337965@qq.com>
2025-05-15 17:59:50 +0800

    延迟加载打断点就消失

--- src/test/java/CountryMapperTest.java
+++ src/test/java/CountryMapperTest.java
@@ -5,8 +5,11 @@ import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.io.Reader;
@@ -14,6 +17,7 @@ import java.util.List;
 
 public class CountryMapperTest {
 
+    private static final Logger log = LoggerFactory.getLogger(CountryMapperTest.class);
     private static SqlSessionFactory sqlSessionFactory;
 
     @BeforeClass
@@ -95,7 +99,9 @@ public class CountryMapperTest {
         try {
             SysUserMapper mapper = sqlSession.getMapper(SysUserMapper.class);
             SysUser sysUser = mapper.selectUserAndRoleByIdSelect(1001L);
-            System.out.println(sysUser.toString());
+            Assert.assertNotNull(sysUser) ;
+//            Assert.assertNotNull(sysUser.getRole()) ;
+            log.info("1");
         } finally {
 //不要忘记关闭 sqlSession
             sqlSession.close();
